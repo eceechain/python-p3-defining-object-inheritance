46c6da53783437e3e35c2cbfea78caf87f7f967b
ecee <you@example.com>
2023-11-27 13:16:42 +0300

    Done

--- lib/car.py
+++ lib/car.py
@@ -3,7 +3,8 @@ from vehicle import Vehicle
 class Car(Vehicle):
     
         def __init__(self, wheel_size, wheel_number):
-            super().__init__(wheel_size, wheel_number)
+              self.wheel_number = wheel_number
+              self.wheel_size = wheel_size
     
         def go(self):
             return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
