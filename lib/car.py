4a129d235a0ee6b56eb4e74dd9f2537025bde927
ecee <you@example.com>
2023-11-28 16:36:15 +0300

    Done

--- lib/car.py
+++ lib/car.py
@@ -1,10 +1,6 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    
-        def __init__(self, wheel_size, wheel_number):
-              self.wheel_number = wheel_number
-              self.wheel_size = wheel_size
-    
-        def go(self):
+                  
+      def go(self):
             return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
